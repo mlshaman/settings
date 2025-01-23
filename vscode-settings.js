da395a7db487c8452a141da10b585e841837d352
MLȘaman aka Andrei [blog account] <135952373+mlshaman@users.noreply.github.com>
2025-01-23 09:53:57 +0200

    Create vscode-settings.js

---
+++ vscode-settings.js
@@ -0,0 +1,90 @@
+// clean py
+{
+    "zenMode.centerLayout": false,
+    "zenMode.fullScreen": false,
+    "zenMode.hideLineNumbers": false,
+    "editor.fontFamily": "'Iosevka Fixed', 'xSF Mono', 'CaskaydiaCove Nerd Font Mono', Menlo, Monaco, 'Courier New', monospace",
+    "editor.fontSize": 13,
+    "workbench.editor.wrapTabs": true,
+    "python.analysis.inlayHints.variableTypes": true,
+    "python.analysis.inlayHints.functionReturnTypes": true,
+    "editor.inlayHints.enabled": "offUnlessPressed",
+    "editor.minimap.enabled": false,
+    "notebook.lineNumbers": "on",
+    "workbench.editor.highlightModifiedTabs": true,
+    "notebook.output.textLineLimit": 500,
+    "notebook.output.scrolling": true,
+    "editor.lineHeight": 1.1,
+    "workbench.colorTheme": "Default Light Modern",
+    "workbench.sideBar.location": "right",
+    "workbench.editor.editorActionsLocation": "titleBar"
+}
+
+// cool py
+{
+    // --- <ZEN-MODE> ---
+    "zenMode.centerLayout": false,
+    "zenMode.fullScreen": false,
+    "zenMode.hideLineNumbers": false,
+    // --- </ZEN-MODE> ---
+    "editor.fontFamily": "'xSF Mono', 'CaskaydiaCove Nerd Font Mono', Menlo, Monaco, 'Courier New', monospace",
+    "editor.fontSize": 13,
+    // --- <AUTOCOMPLETE-SUGGESTIONS-HINTS> ---
+    "python.analysis.inlayHints.variableTypes": true,
+    "python.analysis.inlayHints.functionReturnTypes": true,
+    "editor.inlayHints.enabled": "offUnlessPressed",
+    "workbench.editor.wrapTabs": true,
+    // --- </AUTOCOMPLETE-SUGGESTIONS-HINTS> ---
+    "editor.minimap.enabled": false,
+    "editor.tokenColorCustomizations": {
+        "[GitHub Light]": {
+            "comments": "#40a33d",
+            "strings": "#c44fb4",
+        },
+        "[GitHub Dark]": {
+            "comments": "#69c43f",
+            "strings": "#f16ae6",
+        },
+        "[1337]": {
+            "comments": "#6ec7bf"
+        },
+        "[Nord Wave]": {
+            "comments": "#836dc8",
+        },
+        "[Abyss]": {
+            "comments": "#135d66"
+        },
+        "[Ascetic-Night]": {
+            "strings": "#41762e",
+            "comments": "#71609b",
+            "keywords": {
+                "fontStyle": "bold",
+                "foreground": "#ffffffad"
+            },
+        },
+        "[Gruvbox Dark Hard]": {
+            "comments": {
+                "fontStyle": ""
+            }
+        }
+    },
+    "editor.bracketPairColorization.enabled": false,
+    // --- <NOTEBOOK> ---
+    "notebook.lineNumbers": "on",
+    "notebook.output.textLineLimit": 1000,
+    "notebook.output.scrolling": true,
+    "editor.wordWrap": "on",
+    "notebook.output.wordWrap": true,
+    "workbench.editor.tabSizing": "shrink",
+    "github.copilot.advanced": {
+        "authProvider": "github"
+    },
+    "github.copilot.preferredAccount": "NeuronQ",
+    "notebook.formatOnSave.enabled": true,
+    "jupyter.widgetScriptSources": [
+        "jsdelivr.com",
+        "unpkg.com"
+    ],
+    "workbench.colorTheme": "Default Dark+",
+    // --- </NOTEBOOK> ---
+}
